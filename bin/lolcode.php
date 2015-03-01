de67268ab6fc0b3e03ee1e923c8d58231b6e492e
rdohms <rdohms@gmail.com>
2015-03-01 19:27:06 +0100

    Fixing paths
    Added a few fallbacks to find the proper composer autoloader path

--- bin/lolcode.php
+++ bin/lolcode.php
@@ -1,7 +1,27 @@
 #!/usr/bin/env php
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+foreach (array(
+             __DIR__ . '/../../../autoload.php',
+             __DIR__ . '/vendor/autoload.php',
+             __DIR__ . '/../vendor/autoload.php',
+             __DIR__ . '/../autoload.php',
+         ) as $file) {
+    if (file_exists($file)) {
+        define('DMS_LOLCODE_COMPOSER_AUTOLOAD', $file);
+        break;
+    }
+}
+
+if (!defined('DMS_LOLCODE_COMPOSER_AUTOLOAD')) {
+    die(
+        'You need to set up the project dependencies using the following commands:' . PHP_EOL .
+        'wget http://getcomposer.org/composer.phar' . PHP_EOL .
+        'php composer.phar install' . PHP_EOL
+    );
+}
+
+require DMS_LOLCODE_COMPOSER_AUTOLOAD;
 
 use DMS\LOL\Command\LolCodeCommand;
 use Symfony\Component\Console\Application;
